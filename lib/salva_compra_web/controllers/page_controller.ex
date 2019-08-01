b023f17b2abfa64a7af6a64265882b19668fedbf
daniel calliari <dbcalliari@gmail.com>
2019-08-01 05:45:12 -0300

    Orçamento V1

--- lib/salva_compra_web/controllers/page_controller.ex
+++ lib/salva_compra_web/controllers/page_controller.ex
@@ -13,14 +13,58 @@ defmodule SalvaCompraWeb.PageController do
     })
   end
 
-  def print(conn, %{"usuario" => usuario_params}) do
-    IO.inspect(usuario_params)
+  def print(conn, %{
+        "orcamento" => %{
+          "criacao" => criacao,
+          "validade" => validade,
+          "condicao" => condicao,
+          "telefone" => telefone,
+          "cidade" => cidade,
+          "nome" => nome,
+          "nome_completo" => nome_completo,
+          "uf" => uf,
+          "cpf" => cpf,
+          "email" => email,
+          "ramo" => ramo,
+          "carrinho" => carrinho
+        }
+      }) do
+    data = SalvaCompra.Carrinho.Produtos.produtos()
+
+    produtos =
+      Enum.map(carrinho, fn produto ->
+        item = data[produto["id"]]
+
+        %{
+          nome: item.nome,
+          preco: item.preco,
+          qtd: produto["qtd"],
+          total: item.total,
+          ipi: item.ipi
+        }
+      end)
+
+    total = Enum.reduce(produtos, 0, fn produto, acc -> produto.total + acc end)
 
     html =
       Phoenix.View.render_to_string(SalvaCompraWeb.PageView, "new_pdf.html", %{
         conn: conn,
         ntp: Images64.logo_ntp(),
-        salva: Images64.logo_salva()
+        salva: Images64.logo_salva(),
+        criacao: criacao,
+        validade: validade,
+        condicao: condicao,
+        telefone: telefone,
+        cidade: cidade,
+        nome: nome,
+        nome_completo: nome_completo,
+        uf: uf,
+        cpf: cpf,
+        email: email,
+        ramo: ramo,
+        carrinho: carrinho,
+        produtos: produtos,
+        total: total
       })
 
     # {:ok, filename} = PdfGenerator.generate(html)
