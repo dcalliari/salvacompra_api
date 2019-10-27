5fa73a51e9232f2e90e95fb1374a679631d3b11f
daniel calliari <dbcalliari@gmail.com>
2019-10-27 16:07:22 -0300

    Segunda lista

--- lib/salva_compra_web/templates/orcamento/components.ex
+++ lib/salva_compra_web/templates/orcamento/components.ex
@@ -2,9 +2,12 @@ defmodule SalvaCompraWeb.Components do
   alias SalvaCompraWeb.OrcamentoView
   @page_height 785
   @header_height 30
-  @footer_height 40
+  @footer_height 90
   @valor_table_height 40
   @produto_height 12.75
+  @info_header_height 20
+  @info_item_small_height 12.75
+  @info_item_medium_height 22.51
   def divisor(height) do
     Phoenix.View.render(OrcamentoView, "divisor.html", %{height: height})
   end
@@ -28,8 +31,16 @@ defmodule SalvaCompraWeb.Components do
     Phoenix.View.render(OrcamentoView, "table_header.html", %{})
   end
 
+  def table_info_header() do
+    Phoenix.View.render(OrcamentoView, "table_info_header.html", %{})
+  end
+
   def produto(produto) do
     Phoenix.View.render(OrcamentoView, "produto.html", %{produto: produto})
+  end
+
+  def produto_info(produto) do
+    Phoenix.View.render(OrcamentoView, "produto_info.html", %{produto: produto})
   end
 
   def valor_table(total) do
@@ -76,6 +87,43 @@ defmodule SalvaCompraWeb.Components do
     {height, new_page, [html | valor_html]}
   end
 
+  def info_header({height, new_page, html}) do
+    {info_header_html, height} =
+      if height - @info_header_height < @footer_height do
+        {new_page_html, height} = new_page.(height)
+        info_header_html = table_info_header()
+        {[new_page_html, info_header_html], height}
+      else
+        info_header_html = table_info_header()
+        {[info_header_html], height - @info_header_height}
+      end
+
+    {height, new_page, [html | info_header_html]}
+  end
+
+  def produtos_info({height, new_page, html}, produtos) do
+    {produtos_html, height} =
+      Enum.map_reduce(produtos, height, fn produto, height ->
+        produto_info_height =
+          if String.length(produto.nome) > 20 do
+            @info_item_medium_height
+          else
+            @info_item_small_height
+          end
+
+        if height - produto_info_height < @footer_height do
+          table_header_html = table_header()
+          produto_html = produto_info(produto)
+          {new_page_html, height} = new_page.(height)
+          {[new_page_html, table_header_html, produto_html], height}
+        else
+          {produto_info(produto), height - produto_info_height}
+        end
+      end)
+
+    {height, new_page, [html | produtos_html]}
+  end
+
   def build_page(produtos, height, user, nome, nome_completo, total) do
     new_page_fn = new_page(user, nome, nome_completo)
 
@@ -83,8 +131,10 @@ defmodule SalvaCompraWeb.Components do
       {height, new_page_fn, {:safe, ""}}
       |> produtos(produtos)
       |> valor(total)
+      |> info_header()
+      |> produtos_info(produtos)
 
-    html
+    [html, footer(user, nome, nome_completo)]
   end
 
   # <%=  Enum.map(@produtos, fn produto ->  %>
